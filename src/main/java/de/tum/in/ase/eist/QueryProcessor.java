5c51b3ef591549fb220bba2af0153f0c77362767
Michel <ge47hek@mytum.de>
2022-07-14 15:13:13 +0200

    biggest

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,15 +6,25 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "Michel";
-        } else { // TODO extend the programm here
-            return "";
+            return "Michel";
+        } else if (query.contains("largest")) {
+            int biggest = 0;
+            String[] words = query.split(" ");
+            for (int i = 0; i < words.length; i++) {
+                if (Integer.parseInt(words[i]) > biggest) {
+                    biggest = Integer.parseInt(words[i]);
+                }
+            }
+            return Integer.toString(biggest);
+        } else if (query.contains("plus")) {
+
         }
+        return null;
     }
 }
